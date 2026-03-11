831e34179e55a68e370b8a2a358afeef7a3f626c
Marcelo Nonnenmacher de Souza <marcelosouzars@gmail.com>
2026-03-10 21:09:58 -0300

    Altera gerador de unidades 5

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "59449bb8bb9d2fc2d6f79ee2e323495e
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "77d21f13e2aa499c10b3a4e40c509708",
+"flutter_bootstrap.js": "0a095e1173fd3ef1f82000f4acfcfade",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
